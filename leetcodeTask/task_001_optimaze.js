1791c8df2cdb6b86af871a1b1d374a083fd0de34
Stepan <stepan.sharifulin@mail.ru>
2023-06-10 23:51:08 +0500

    Add task 001 optimaze solve

---
+++ leetcodeTask/task_001_optimaze.js
@@ -0,0 +1,16 @@
+// let nums = [2,7,11,15], target = 9
+let nums = [3,2,4], target = 6
+
+var twoSum = function(nums, target) {
+    const hashTable = {}
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+        if (diff in hashTable) {
+            return [hashTable[diff], i]
+        }
+        hashTable[nums[i]] = i;
+    }
+    return [];
+};
+
+console.log((twoSum(nums, target)))
