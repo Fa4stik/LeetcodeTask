692e26dac389a0b39544286b816117ae6a738cb3
Stepan <stepan.sharifulin@mail.ru>
2023-06-18 11:06:00 +0500

    Solve "Remove Duplicates from Sorted Array"

--- leetcodeTask/task_011.js
+++ leetcodeTask/task_011.js
@@ -1,22 +1,23 @@
-const nums = [1,1,2] // 2, nums = [1,2,_]
-// const nums = [0,0,1,1,1,2,2,3,3,4] // 5, nums = [0,1,2,3,4,_,_,_,_,_]
+// const nums = [1,1,2] // 2, nums = [1,2,_]
+const nums = [0,0,1,1,1,2,2,3,3,4] // 5, nums = [0,1,2,3,4,_,_,_,_,_]
+// const nums = [1,2] // 2, nums = [1,2]
+
+// lastNum = 0, i = 1
+// nums[0] !== nums[1] ? nums[1] = null; i++
+// nums[0] !== nums[2] ? nums[++lastNum] = nums[2]
 
 // Нужно изменять напрямую nums и возвращать число уникальных чисел
 var removeDuplicates = function(nums) {
     let lastNum = 0;
-    const nonDuplicateArray = []
-    for (let num of nums) {
-        if (lastNum === num)
-            continue;
-        lastNum = num;
-        nonDuplicateArray.push(num);
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[lastNum] !== nums[i])
+            nums[++lastNum] = nums[i]
     }
-    return {
-        s: nonDuplicateArray.length,
-        nonDuplicateArray
-    };
+    return lastNum+1;
 };
 
 module.exports = () => {
-    return removeDuplicates(nums);
+    const value = removeDuplicates(nums);
+    console.log(nums)
+    return value
 }
