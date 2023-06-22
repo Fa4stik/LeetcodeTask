bb0a9fa1d57c389f673f16810d605b368ffdec9f
Stepan <stepan.sharifulin@mail.ru>
2023-06-23 03:41:26 +0500

    Solve "Convert Binary Number in a Linked List to Integer"

--- leetcodeTask/task_013.js
+++ leetcodeTask/task_013.js
@@ -1,3 +1,25 @@
+// const head = [1,0,1]
+// const head = [0]
+// const head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ * */
+
+var getDecimalValue = function(head) {
+    let decimalNumber = 0, currentNode = head;
+    while (currentNode) {
+        decimalNumber += currentNode.val;
+        decimalNumber*=2;
+        currentNode = currentNode.next;
+    }
+    return decimalNumber/2;
+};
+
 module.exports = () => {
-    return 1;
+    return getDecimalValue(head);
 }
